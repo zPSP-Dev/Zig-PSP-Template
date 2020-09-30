664a6fd2e4c71e3df1d98bd6b2ed0e9a68dda4c3
Nathan Bourgeois <iridescentrosesfall@gmail.com>
2020-09-30 05:27:28 -0400

    Updated template!

--- src/main.zig
+++ src/main.zig
@@ -1,12 +1,7 @@
-//A simple hello world
-
-//We have 2 different ways of including the PSPSDK - either the minimalist version within utils (you can later include other modules...)
-//Or the full SDK through pspsdk.zig with all libraries.
-//In a minimalist instance, binaries are 13,284 bytes on release safe versus 22,724 bytes on the full version (no GU)
 const psp = @import("Zig-PSP/src/psp/utils/psp.zig");
 
 comptime {
-    _ = psp.module_start_struct;
+    asm(psp.module_info("Zig PSP", 0, 1, 0));
 }
 
 pub fn main() !void {
